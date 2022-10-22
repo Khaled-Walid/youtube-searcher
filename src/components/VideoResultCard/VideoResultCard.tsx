1adf8b4438ba86aa856ec81fda000204cfbd664a
Khaled-Walid <justkhwf@outlook.com>
2022-10-23 01:46:18 +0200

    add timer to VideoResultCard

--- src/components/VideoResultCard/VideoResultCard.tsx
+++ src/components/VideoResultCard/VideoResultCard.tsx
@@ -6,6 +6,7 @@ export default function VideoResultCard():JSX.Element {
     <a href="https://www.link.com" className="video-result-card" target="_blank" rel="noreferrer">
       <div className="left-side">
         <img className="thumbnail" src="https://i.ytimg.com/vi/1CptfMEEC8g/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDwRXBXzlEglP3_Sj4W2GRWZs-NVw" alt="video thumbnail" />
+        <span className="timer">1:20:35</span>
       </div>
       <div className="right-side">
         <h3 className="clamp-text title">Video Title</h3>
